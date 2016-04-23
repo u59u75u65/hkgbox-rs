ef57cc77ff8abe6ef56bd08964461383d4aa7e67
u59u75u65 <merrymenfolks@gmail.com>
2016-04-23 20:56:04 +0800

    extract client module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
-extern crate hyper;
+extern crate hkg;
 
-use std::io::Read;
-
-use hyper::Client;
-use hyper::header::Connection;
+use hkg::utility::client::download;
 
 fn main(){
+
     let body = download();
     println!("Response: {}", body);
-}
 
-fn download() -> String{
-        // Create a client.
-        let client = Client::new();
-
-        // Creating an outgoing request.
-        let mut res = client.get("http://forum1.hkgolden.com/topics.aspx?type=BW")
-            // set a header
-            .header(Connection::close())
-            // let 'er go!
-            .send().unwrap();
-
-        // Read the Response.
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-
-    return body;
 }
