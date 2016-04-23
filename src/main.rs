b26563706eaf1b675c22c5e248ebb352a19ca3b1
u59u75u65 <merrymenfolks@gmail.com>
2016-04-23 21:04:39 +0800

    add readfile function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate hkg;
 
 use hkg::utility::client::download;
+use hkg::utility::cache::readfile;
 
 fn main(){
 
-    let body = download();
-    println!("Response: {}", body);
-
+    // let body = download();
+    // println!("Response: {}", body);
+    let s = readfile("demo.html".to_string());
+    print!("contains:\n{}", s)
 }
