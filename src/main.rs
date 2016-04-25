83166a4e3faf8a58310660dd4dda69be1a4b7448
u59u75u65 <merrymenfolks@gmail.com>
2016-04-26 01:19:18 +0800

    print body

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,10 @@ fn main(){
     let w = rustbox.width();
     let h = rustbox.height();
     let title = String::from("高登");
-    print_header(&rustbox, title, w, h);
+    print_header(&rustbox, w, h, title);
+    print_body(&rustbox, w, h, 2, 2);
+    rustbox.print(1, 23, rustbox::RB_BOLD, Color::White, Color::Black, "Press 'q' to quit.");
 
-    rustbox.print(1, 5, rustbox::RB_BOLD, Color::White, Color::Black, "Press 'q' to quit.");
 
     loop {
         rustbox.present();
@@ -36,10 +37,25 @@ fn main(){
     }
 }
 
-fn print_header(rustbox: &rustbox::RustBox, text: String, width: usize, height: usize)
+
+fn print_header(rustbox: &rustbox::RustBox, width: usize, height: usize, text: String)
 {
     let padding = (width - text.len())/2;
     let header_bottom = (0..width).map(|i|"─").collect::<Vec<_>>().join("");
     rustbox.print(padding, 0, rustbox::RB_BOLD, Color::White, Color::Black, &text);
     rustbox.print(0, 1, rustbox::RB_BOLD, Color::Yellow, Color::Black, &header_bottom);
+}
+
+fn print_body(rustbox: &rustbox::RustBox, width: usize, height: usize, offset_x: usize, rows: usize)
+{
+    let titles = vec!["紅魔英超睇敢帥　十分之高招", "發覺好多後生仔女搭火車地鐵 有位都唔坐"];
+    let authors = vec!["電超","程詠樂"];
+
+    for i in (0..rows)
+    {
+        rustbox.print(0, i  + offset_x, rustbox::RB_BOLD, Color::White, Color::Black, &format!("{no:>2}|{title:<50}|{author}",
+                                                                                                                        no = i+1,
+                                                                                                                        title = &titles[i],
+                                                                                                                        author = &authors[i]));
+    }
 }
