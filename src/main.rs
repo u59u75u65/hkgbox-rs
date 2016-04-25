282682528cfdc344a8f29386b10506fbcde9eb2b
u59u75u65 <merrymenfolks@gmail.com>
2016-04-26 02:16:46 +0800

    working draft

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,25 @@ fn main() {
     let h = rustbox.height();
     let title = String::from("高登");
     print_header(&rustbox, w, h, title);
-    // print_body(&rustbox, w, h, 2, 2);
-    // rustbox.print(1, 23, rustbox::RB_BOLD, Color::White, Color::Black, "Press 'q' to quit.");
+    print_body(&rustbox, w, h, 2, 2);
+    rustbox.print(1,
+                  23,
+                  rustbox::RB_BOLD,
+                  Color::White,
+                  Color::Black,
+                  "Press 'q' to quit.");
 
-    print_cjk_count(&rustbox);
+    // print_cjk_count(&rustbox);
+    //
+    // let s2 = String::from("<<100%成功率>>如何成為成功?香港Youtuber");
+    //
+    // rustbox.print(1,
+    //               3,
+    //               rustbox::RB_BOLD,
+    //               Color::White,
+    //               Color::Black,
+    //               &format!("{}", real_count(s2)));
+
 
     loop {
         rustbox.present();
@@ -59,6 +74,18 @@ fn contains(c: char) -> bool {
     return false;
 }
 
+fn real_count(s: &str) -> usize {
+    return s.chars()
+            .map(|x| if contains(x) {
+                2
+            } else {
+                1
+            })
+            .collect::<Vec<usize>>()
+            .iter()
+            .fold(0, |acc, &x| acc + x);
+}
+
 fn print_header(rustbox: &rustbox::RustBox, width: usize, height: usize, text: String) {
     let padding = (width - text.len()) / 2;
     let header_bottom = (0..width).map(|i| "─").collect::<Vec<_>>().join("");
@@ -85,20 +112,34 @@ fn print_body(rustbox: &rustbox::RustBox,
                       "發覺好多後生仔女搭火車地鐵 有位都唔坐"];
     let authors = vec!["電超", "程詠樂"];
 
+    let mut title_spacing = 0;
     for i in (0..rows) {
+
+
+        let s1 = format!("{no:>2}|", no = i + 1);
+        let s2 = format!("{title}", title = &titles[i]);
+
         rustbox.print(0,
                       i + offset_x,
                       rustbox::RB_BOLD,
                       Color::White,
                       Color::Black,
-                      &format!("{no:>2}|{title:<50}|{author}",
-                               no = i + 1,
-                               title = &titles[i],
-                               author = &authors[i]));
+                      &(format!("{}{}", s1,s2)));
+
+        title_spacing = s1.len() + real_count(&titles[i]) + 55 - real_count(&titles[i]);
+
+        rustbox.print(title_spacing,
+                      i + offset_x,
+                      rustbox::RB_BOLD,
+                      Color::White,
+                      Color::Black,
+                      &format!("|{author}", author = &authors[i]));
     }
 }
 
-fn print_cjk_count(rustbox: &rustbox::RustBox){
+
+
+fn print_cjk_count(rustbox: &rustbox::RustBox) {
 
     let mut offset_y = 2;
 
@@ -124,7 +165,7 @@ fn print_cjk_count(rustbox: &rustbox::RustBox){
                           Color::White,
                           Color::Black,
                           &format!("[{:<2}] {:>2} 0x{:X} {}", d + offset_y, c, c as u32, &"YES"));
-                          s2count = s2count + 2;
+            s2count = s2count + 2;
         } else {
             rustbox.print(1,
                           d + offset_y,
@@ -132,11 +173,19 @@ fn print_cjk_count(rustbox: &rustbox::RustBox){
                           Color::White,
                           Color::Black,
                           &format!("[{:<2}] {:>2} 0x{:X} {}", d + offset_y, c, c as u32, &"NO"));
-                          s2count = s2count + 1;
+            s2count = s2count + 1;
         }
     }
 
-    let sum = s2.chars().map(|x| if contains(x) { 2 } else { 1 } ).collect::<Vec<u32>>().iter().fold(0, |acc, &x| acc + x);
+    let sum = s2.chars()
+                .map(|x| if contains(x) {
+                    2
+                } else {
+                    1
+                })
+                .collect::<Vec<u32>>()
+                .iter()
+                .fold(0, |acc, &x| acc + x);
 
     rustbox.print(1,
                   45,
