f0715dc16d1d5a4f6c3940fae300a0e977b1c925
u59u75u65 <merrymenfolks@gmail.com>
2016-04-25 22:20:50 +0800

    simple print header

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,45 @@
 extern crate hkg;
 
-extern crate kuchiki;
-use kuchiki::traits::*;
+extern crate rustbox;
 
-use hkg::utility::client::download;
-use hkg::utility::cache::readfile;
+use std::default::Default;
+
+use rustbox::{Color, RustBox};
+use rustbox::Key;
 
 fn main(){
 
-    // let body = download();
-    // println!("Response: {}", body);
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
 
-    // let html = readfile("demo.html".to_string());
-    // print!("contains:\n{}", html)
+    let w = rustbox.width();
+    let h = rustbox.height();
+    let title = String::from("高登");
+    print_header(&rustbox, title, w, h);
 
-    let path = "demo.html";
+    rustbox.print(1, 5, rustbox::RB_BOLD, Color::White, Color::Black, "Press 'q' to quit.");
 
-    let document = kuchiki::parse_html().from_utf8().from_file(&path).unwrap();
-    // println!("{:?}", document.to_string());
-
-    let topic_list_panel = document.select(".Topic_ListPanel").unwrap().collect::<Vec<_>>();
-
-    if topic_list_panel.len() >  0 {
-        let trs = topic_list_panel[0].as_node().select("tr").unwrap().collect::<Vec<_>>();
-        println!("trs len: {:?}", trs.len());
-
-        for (index, tr) in trs.iter().enumerate() {
-            let tr_node = tr.as_node();
-            // println!("{:?}", tr_node.text_contents());
-
-            let tds = tr_node.select("td").unwrap().collect::<Vec<_>>();
-            println!("tds[{:?}]={:?}", index, tds.len());
+    loop {
+        rustbox.present();
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => {
+                match key {
+                    Key::Char('q') => { break; }
+                    _ => { }
+                }
+            },
+            Err(e) => panic!("{}", e),
+            _ => { }
         }
     }
-    else
-    {
-        println!("not found");
-    }
+}
 
+fn print_header(rustbox: &rustbox::RustBox, text: String, width: usize, height: usize)
+{
+    let padding = (width - text.len())/2;
+    let header_bottom = (0..width).map(|i|"─").collect::<Vec<_>>().join("");
+    rustbox.print(padding, 0, rustbox::RB_BOLD, Color::White, Color::Black, &text);
+    rustbox.print(0, 1, rustbox::RB_BOLD, Color::Yellow, Color::Black, &header_bottom);
 }
