20c34caf8b51bd5f9c5bccd1802f676555cffd00
u59u75u65 <merrymenfolks@gmail.com>
2016-05-11 01:40:46 +0800

    extract parse show item to parser class

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 extern crate rustc_serialize;
 extern crate rustbox;
 extern crate chrono;
+extern crate kuchiki;
+extern crate regex;
 
 pub mod utility;
 pub mod model;
+pub mod parser;
 pub mod screen;
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate rustbox;
 extern crate rustc_serialize;
 extern crate chrono;
 extern crate kuchiki;
-extern crate regex;
 
 use kuchiki::traits::*;
 use kuchiki::NodeRef;
@@ -30,8 +29,6 @@ use std::io::{Error, ErrorKind};
 
 use std::io::Cursor;
 use std::io::BufReader;
-
-use regex::Regex;
 
 #[derive(PartialEq, Eq, Copy, Clone)]
 enum Status {
@@ -78,6 +75,7 @@ fn main() {
     let mut list = hkg::screen::list::List::new(&rustbox);
     let mut show = hkg::screen::show::Show::new(&rustbox);
 
+    let mut parser = hkg::parser::Parser::new();
     loop {
 
         // show UI
@@ -105,7 +103,7 @@ fn main() {
 
         let document = kuchiki::parse_html().from_utf8().from_file(&path1).unwrap();
 
-        let si = parse_show_item(&document);
+        let si = parser.show_item(&document);
 
         rustbox.print(1,
                       5,
@@ -317,115 +315,6 @@ fn main() {
             _ => {}
         }
     }
-}
-
-fn parse_show_item(document: &NodeRef) -> ShowItem {
-
-    let (title, reply_count) = {
-        let repliers_tr = document.select(".repliers tr").unwrap().next().unwrap();
-        let repliers_header = repliers_tr.as_node()
-                                         .select(".repliers_header")
-                                         .unwrap()
-                                         .last()
-                                         .unwrap();
-        let divs = repliers_header.as_node().select("div").unwrap().collect::<Vec<_>>();
-
-        let topic_data = divs.iter()
-                             .enumerate()
-                             .map(|(index, div)| {
-                                 let s_trimmed = div.text_contents().trim().to_string();
-                                 if index == 1 {
-                                     let re = Regex::new(r"^(?P<count>\d+)個回應$").unwrap();
-                                     let cap = re.captures(&s_trimmed).unwrap();
-                                     // String::from(cap.name("count").unwrap_or("0"))
-                                     cap.name("count").unwrap_or("0").to_string()
-                                 } else {
-                                     s_trimmed
-                                 }
-                             })
-                             .collect::<Vec<_>>();
-
-        if topic_data.len() < 2 {
-            panic!("length of topic_data is invalid.")
-        }
-
-        (topic_data.get(0).unwrap().to_string(), // return as title
-         topic_data.get(1).unwrap().to_string() /* return as reply_count */)
-    };
-
-    let (page, max_page) = {
-
-        let page_select = document.select("select[name='page']").unwrap().last().unwrap();
-        let page_str = page_select.as_node()
-                                  .select("option[selected='selected']")
-                                  .unwrap()
-                                  .next()
-                                  .unwrap();
-        let max_page_str = page_select.as_node().select("option").unwrap().last().unwrap();
-
-        let page = page_str.text_contents().trim().to_string().parse::<usize>().unwrap_or(0);
-        let max_page = max_page_str.text_contents()
-                                   .trim()
-                                   .to_string()
-                                   .parse::<usize>()
-                                   .unwrap_or(0);
-
-        (page, max_page)
-    };
-
-
-    let replies = parse_show_reply_items(&document);
-
-    ShowItem {
-        url_query: UrlQueryItem { message: String::from("") },
-        replies: replies,
-        page: page,
-        max_page: max_page,
-        reply_count: String::from(reply_count),
-        title: String::from(title),
-    }
-}
-
-fn parse_show_reply_items(document: &NodeRef) -> Vec<ShowReplyItem> {
-
-    let replies_data = document.select(".repliers tr[userid][username]")
-                               .unwrap()
-                               .collect::<Vec<_>>();
-
-    replies_data.iter()
-                .enumerate()
-                .map(|(index, tr)| {
-
-                    let tr_attrs = (&tr.attributes).borrow();
-                    let userid = tr_attrs.get("userid").unwrap();
-                    let username = tr_attrs.get("username").unwrap();
-
-                    let content_elm = tr.as_node()
-                                        .select(".repliers_right .ContentGrid")
-                                        .unwrap()
-                                        .next()
-                                        .unwrap(); // first
-
-                    let mut buff = Cursor::new(Vec::new());
-                    let serialize_result = content_elm.as_node().serialize(&mut buff);
-                    let vec = buff.into_inner();
-                    let content = String::from_utf8(vec).unwrap();
-
-                    let datatime = tr.as_node()
-                                     .select(".repliers_right span")
-                                     .unwrap()
-                                     .last()
-                                     .unwrap()
-                                     .text_contents();
-
-                    ShowReplyItem {
-                        userid: String::from(userid),
-                        username: String::from(username),
-                        content: String::from(content),
-                        published_at: String::from(datatime),
-                    }
-                })
-                .collect::<Vec<_>>()
 }
 
 fn print_status(rustbox: &rustbox::RustBox, status: &str) {
