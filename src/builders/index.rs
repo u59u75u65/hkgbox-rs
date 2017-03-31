fd3061bd4c0167c85bd8f8497f99d8a2341d5ac5
u59u75u65 <merrymenfolks@gmail.com>
2017-04-01 00:21:06 +0800

    return result trait from index builder

--- src/builders/index.rs
+++ src/builders/index.rs
@@ -8,17 +8,14 @@ use kuchiki::ElementData;
 use model::ListTopicItem;
 use model::ListTopicTitleItem;
 use model::ListTopicAuthorItem;
-use model::ShowItem;
-use model::ShowReplyItem;
 use model::UrlQueryItem;
-use reply_model::*;
 
 use regex::Regex;
 use url::Url;
 use std::collections::HashMap;
 
 #[derive(Clone)]
-pub struct Index { }
+pub struct Index {}
 
 impl Index {
     pub fn new() -> Self {
@@ -26,61 +23,103 @@ impl Index {
     }
     pub fn build(&mut self, document: &NodeRef) -> Result<Vec<ListTopicItem>, &'static str> {
 
-            let trs_option = document.select(".Topic_ListPanel tr[id]");
+        let trs_option = document.select(".Topic_ListPanel tr[id]");
 
-            if trs_option.is_err() {
-                return Err("fail to parse list topics item, reaons: trs_option not found");
-            }
+        if trs_option.is_err() {
+            return Err("fail to parse list topics item, reaons: trs_option not found");
+        }
 
-            let trs = trs_option.unwrap();
+        let trs = trs_option.unwrap();
 
-            let list_topics = trs.enumerate().map(list_topic_items_handler).collect::<Vec<_>>();
+        let list_topics = trs.enumerate().map(list_topic_items_handler).collect::<Vec<_>>();
 
-            let err_list_topics_option = list_topics.iter().filter(|x| x.is_err()).next();
+        let err_list_topics_option = list_topics.iter().filter(|x| x.is_err()).next();
 
-            if err_list_topics_option.is_some() {
-                return Err(&"fail to parse list topics items, reason: 'error list topic item' was found");
-            }
+        if err_list_topics_option.is_some() {
+            return Err(&"fail to parse list topics items, reason: 'error list topic item' was found");
+        }
 
-            let result = list_topics.iter().map(|x| x.clone().unwrap() ).collect::<Vec<_>>();
+        let result = list_topics.iter().map(|x| x.clone().unwrap()).collect::<Vec<_>>();
 
-            Ok(result)
+        Ok(result)
     }
 }
 
-fn list_topic_items_handler((index,tr): (usize, ::kuchiki::NodeDataRef<::kuchiki::ElementData>)) -> Result<ListTopicItem, &'static str> {
+fn list_topic_items_handler((index, tr): (usize, ::kuchiki::NodeDataRef<::kuchiki::ElementData>)) -> Result<ListTopicItem, &'static str> {
 
-    let items = match tr.as_node().select("td") {
-        Ok(items) => items,
-        Err(e) => panic!("{:?}", e)
-    };
+    let items_option = tr.as_node().select("td");
+
+    if items_option.is_err() {
+        return Err("fail to parse list topic items, reason: items_option not found");
+    }
+
+    let items = items_option.unwrap();
 
     let mut result: ListTopicItem = Default::default();
 
     for (j, item) in items.enumerate().filter(|&(j, _)| j > 0 && j < 6) {
         match j {
-            1 => { result.title = parse_list_topic_title_item(&item) },
-            2 => { result.author = parse_list_topic_author_item(&item) },
+            1 => {
+                match parse_list_topic_title_item(&item) {
+                    Ok(s) => result.title = s,
+                    Err(_) => {}
+                }
+            }
+            2 => {
+                match parse_list_topic_author_item(&item) {
+                    Ok(s) => result.author = s,
+                    Err(_) => {}
+                }
+            }
             3 => {
                 let (date, time) = {
                     let text = item.text_contents().trim().to_string();
                     let map = text.split("\n").map(|x| x.trim().to_string()).collect::<Vec<_>>();
-                    if map.len() < 2 {
-                        panic!("length of map is invalid.");
+                    let count = map.len();
+
+                    if count < 2 {
+                        error!("length of map is invalid. length: {}", count);
+                        return Err(&"length of map is invalid.");
                     }
-                    (map[0].clone(), map[1].clone())
+
+                    let mut map_enumerator = map.iter().enumerate();
+                    let date_option = match map_enumerator.clone()
+                              .filter(|&(i, _)| i == 0)
+                              .map(|(i, e)| (i, e))
+                              .next() {
+                        Some((i, text)) => Some(text),
+                        None => None,
+                    };
+
+                    let time_option = match map_enumerator.clone()
+                              .filter(|&(i, _)| i == 1)
+                              .map(|(i, e)| (i, e))
+                              .next() {
+                        Some((i, text)) => Some(text),
+                        None => None,
+                    };
+
+                    if date_option.is_none() {
+                        return Err("fail to parse list topic items, reason: date_option not found");
+                    }
+
+                    if time_option.is_none() {
+                        return Err("fail to parse list topic items, reason: time_option not found");
+                    }
+
+                    (date_option.unwrap().clone(), time_option.unwrap().clone())
                 };
                 result.last_replied_date = date;
                 result.last_replied_time = time;
-            },
+            }
             4 => {
                 let text = item.text_contents().trim().to_string();
                 result.reply_count = text
-            },
+            }
             5 => {
                 let text = item.text_contents().trim().to_string();
                 result.rating = text
-            },
+            }
             _ => {}
         }
     }
@@ -89,27 +128,33 @@ fn list_topic_items_handler((index,tr): (usize, ::kuchiki::NodeDataRef<::kuchiki
 
 }
 
-fn parse_list_topic_title_item(item: &NodeDataRef<ElementData>) -> ListTopicTitleItem {
+fn parse_list_topic_title_item(item: &NodeDataRef<ElementData>) -> Result<ListTopicTitleItem, &'static str> {
     let (first_link, links_count) = {
-        let mut links = match item.as_node().select("a") {
-            Ok(links) => links,
-            Err(e) => panic!("ERR: {:?}", e)
-        };
+        let mut links_option = item.as_node().select("a");
+
+        if links_option.is_err() {
+            return Err("fail to parse list topic title item, reason: links not found");
+        }
+
+        let mut links = links_option.unwrap();
 
         let first_link_option = links.next();
         let last_link_option = links.last();
 
-        let first_link = match first_link_option {
-            Some(first_link) => first_link,
-            None => { panic!("ERR: Can't find 'first_link'.") }
-        };
+        if first_link_option.is_none() {
+            return Err("fail to parse list topic title item, reason: first_link_option not found");
+        }
+        let first_link = first_link_option.unwrap();
 
         let max_page = match last_link_option {
-            Some(last_link) =>
-                last_link.text_contents().trim().to_string().parse::<usize>()
-                .unwrap_or(0)
-            ,
-            None => { 1 }
+            Some(last_link) => {
+                last_link.text_contents()
+                    .trim()
+                    .to_string()
+                    .parse::<usize>()
+                    .unwrap_or(0)
+            }
+            None => 1,
         };
 
         (first_link, max_page)
@@ -119,42 +164,59 @@ fn parse_list_topic_title_item(item: &NodeDataRef<ElementData>) -> ListTopicTitl
         let attrs = &(first_link.attributes).borrow();
         let href = attrs.get("href").unwrap_or("");
 
-        let base_url = match Url::parse("http://forum1.hkgolden.com/view.aspx") {
-            Ok(url) => url,
-            Err(e) => { panic!(format!("fail to parse Base URL. Reason: {}", e)) }
-        };
-        let url = match base_url.join(&href) {
-            Ok(url) => url,
-            Err(e) => { panic!(format!("fail to build URL. Reason: {}", e)) }
-        };
+        let base_url_option = Url::parse("http://forum1.hkgolden.com/view.aspx");
+
+        if base_url_option.is_err() {
+            return Err("fail to parse list topic title item, reason: base_url_option not found");
+        }
+        let base_url = base_url_option.unwrap();
+
+        let url_result = base_url.join(&href);
+
+        if url_result.is_err() {
+            return Err("fail to parse list topic title item, reason: fail to build URL. ");
+        }
+        let url = url_result.unwrap();
+
         let url_str = url.into_string();
-        let url_query_item = parse_url_query_item(&url_str);
-        (url_str, url_query_item)
+        let url_query_option = parse_url_query_item(&url_str);
+
+        if url_query_option.is_err() {
+            return Err("fail to parse list topic title item, reason: fail to parse url query");
+        }
+
+        let url_query = url_query_option.unwrap();
+
+        (url_str, url_query)
     };
 
     let text = first_link.text_contents().trim().to_string();
 
-     ListTopicTitleItem {
-        url: url_str,
-        url_query: url_query_item,
-        text: text,
-        num_of_pages: links_count
-    }
+    Ok(ListTopicTitleItem {
+           url: url_str,
+           url_query: url_query_item,
+           text: text,
+           num_of_pages: links_count,
+       })
 }
 
-fn parse_list_topic_author_item(item: &NodeDataRef<ElementData>) -> ListTopicAuthorItem {
+fn parse_list_topic_author_item(item: &NodeDataRef<ElementData>) -> Result<ListTopicAuthorItem, &'static str> {
     let (url, name) = {
-        let mut links = match item.as_node().select("a") {
-            Ok(links) => links,
-            Err(e) => panic!("ERR: {:?}", e)
-        };
+
+        let links_option = item.as_node().select("a");
+
+        if links_option.is_err() {
+            return Err("fail to parse list topic author item, reason: links_option not found");
+        }
+
+        let mut links = links_option.unwrap();
 
         let first_link_option = links.next();
 
-        let first_link = match first_link_option {
-            Some(first_link) => first_link,
-            None => { panic!("ERR: Can't find 'first_link'.") }
-        };
+        if first_link_option.is_none() {
+            return Err("fail to parse list topic title item, reason: first_link_option not found");
+        }
+        let first_link = first_link_option.unwrap();
 
         let attrs = &(first_link.attributes).borrow();
         let href = attrs.get("href").unwrap_or("").to_string();
@@ -162,15 +224,26 @@ fn parse_list_topic_author_item(item: &NodeDataRef<ElementData>) -> ListTopicAut
         (href, text)
     };
 
-    ListTopicAuthorItem {
-       url: url,
-       name: name
-    }
+    Ok(ListTopicAuthorItem {
+           url: url,
+           name: name,
+       })
 }
 
-fn parse_url_query_item(url_str: &str) -> UrlQueryItem {
-    let url = Url::parse(&url_str).expect("fail to parse url query item, reason: invalid url");
-    let query = url.query().expect("fail to parse url query item, reason: invalid url query");
+fn parse_url_query_item(url_str: &str) -> Result<UrlQueryItem, &'static str> {
+
+    let url_option = Url::parse(&url_str);
+    if url_option.is_err() {
+        return Err("fail to parse url query item, reason: invalid url");
+    }
+    let url = url_option.unwrap();
+
+    let query_option = url.query();
+    if query_option.is_none() {
+        return Err("fail to parse url query item, reason: invalid url query");
+    }
+    let query = query_option.unwrap();
+
     let re = Regex::new(r"(\\?|&)(?P<key>[^&=]+)=(?P<value>[^&]+)").expect("fail to parse url query item, reason: invalid regex");
 
     let (channel, message) = {
@@ -183,19 +256,27 @@ fn parse_url_query_item(url_str: &str) -> UrlQueryItem {
             map.entry(key).or_insert(value);
         }
 
-        if map.len() < 2 {
-            panic!("length of map is invalid.")
+        let count = map.len();
+        if count < 2 {
+            error!("length of map is invalid. length: {}", count);
+            return Err(&"length of map is invalid.");
         }
 
-        (
-            map.get("type").expect("fail to parse url query item, reason: can not get value of 'type' attribute").to_string(),
-            map.get("message").expect("fail to parse url query item, reason: can not get value of 'message' attribute").to_string()
-        )
+        let type_option = map.get("type");
+        if type_option.is_none() {
+            return Err(&"fail to parse url query item, reason: can not get value of 'type' attribute");
+        }
+
+        let message_option = map.get("message");
+        if message_option.is_none() {
+            return Err(&"fail to parse url query item, reason: can not get value of 'message' attribute");
+        }
+
+        (type_option.unwrap().to_string(), message_option.unwrap().to_string())
     };
 
-    UrlQueryItem {
-        channel: String::from(channel),
-        message: String::from(message)
-    }
-
+    Ok(UrlQueryItem {
+           channel: String::from(channel),
+           message: String::from(message),
+       })
 }
