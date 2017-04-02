51d895bb118eebe86a0277f48e62325780f35f0b
u59u75u65 <merrymenfolks@gmail.com>
2017-04-02 17:46:30 +0800

    timeout without cancelation token source

--- src/resources/image_resource.rs
+++ src/resources/image_resource.rs
@@ -17,12 +17,15 @@ use cancellation::{CancellationToken, CancellationTokenSource, OperationCanceled
 
 use std::sync::mpsc::{Receiver, Sender};
 
-use std::sync::{Arc};
+use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
 
 use caches::file_cache::*;
 
 use crossbeam::*;
+
+use std::thread::park_timeout;
+use std::time::{Instant, Duration};
 
 pub struct ImageResource<'a, T: 'a + Cache + Send> {
     cache: &'a mut Box<T>,
@@ -60,47 +63,65 @@ impl<'a, T: 'a + Cache + Send> Resource for ImageResource<'a, T> {
                             Err(_) => {
                                 info!("image resource - find in cache fail. url:  {}", url2.clone());
 
-                                let ct = CancellationTokenSource::new();
-                                ct.cancel_after(::std::time::Duration::new(2, 0));
 
                                 let (tx_req, rx_req) = channel::<Option<(bool, Vec<u8>, String)>>();
+                                let tx_req2 = tx_req.clone();
 
                                 let url3 = url2.clone();
+                                let url4 = url2.clone();
                                 let ssl = NativeTlsClient::new().unwrap();
                                 let connector = HttpsConnector::new(ssl);
                                 let mut headers = Headers::new();
                                 headers.set(UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36".to_owned()));
 
+                                let beginning_park = Instant::now();
+                                let timeout = Duration::from_secs(2);
+
+                                let mut is_done = Arc::new(AtomicBool::new(false));
+                                let mut is_done2 = is_done.clone();
                                 ::crossbeam::scope(|scope| {
                                     scope.spawn(move ||{
-                                        let th = thread::current();                                        
-                                        ct.run(|| { th.unpark(); }, || {
+                                        let th = thread::current();
 
-                                            let client = Client::with_connector(connector);
-                                            match client.get(&url3).headers(headers).send() {
-                                                Ok(mut resp) => {
-                                                        info!("image resource - http request success url:  {}", url3.clone());
-                                                        let mut buffer = Vec::new();
-                                                        resp.read_to_end(&mut buffer).expect("fail to read buffer from the http response");
-                                                        self.cache.write(&img_path, &img_file_name, buffer.clone()).expect("fail to write cache");
-                                                        tx_req.send(Some( (false, buffer, "".to_string()) ) );
-                                                    }
-                                                Err(e) => {
-                                                    info!("image resource - http request fail url:  {}", url3.clone());
-                                                    tx_req.send( Some( (false, Vec::new(), e.to_string()) ) );
+                                        let client = Client::with_connector(connector);
+                                        match client.get(&url3).headers(headers).send() {
+                                            Ok(mut resp) => {
+                                                    is_done.store(true, Ordering::Relaxed);
+                                                    info!("image resource - http request success url:  {}", url3.clone());
+                                                    let mut buffer = Vec::new();
+                                                    resp.read_to_end(&mut buffer).expect("fail to read buffer from the http response");
+                                                    self.cache.write(&img_path, &img_file_name, buffer.clone()).expect("fail to write cache");
+                                                    tx_req.send(Some( (false, buffer, "".to_string()) ) );
                                                 }
+                                            Err(e) => {
+                                                is_done.store(true, Ordering::Relaxed);
+                                                info!("image resource - http request fail url:  {}", url3.clone());
+                                                tx_req.send( Some( (false, Vec::new(), e.to_string()) ) );
                                             }
-                                        });
-
-                                        if ct.is_canceled() {
-                                            warn!("image request {} is canceled!", url3.clone());
-                                            thread::park_timeout(::std::time::Duration::from_secs(0));
-                                            tx_req.send(None);
-                                            Err(OperationCanceled)
-                                        } else {
-                                            Ok(())
                                         }
                                     });
+                                    scope.spawn(move || {
+                                        while beginning_park.elapsed() < timeout {
+                                            let timeout = timeout - beginning_park.elapsed();
+                                            park_timeout(timeout);
+                                        }
+                                        match Arc::try_unwrap(is_done2) {
+                                            Ok(o) => {
+                                                let is_done_flag = o.load(Ordering::Relaxed);
+                                                warn!("is done flag: {} url: {}", is_done_flag, url4.clone());
+                                                if !is_done_flag {
+                                                    warn!("image request {} is canceled!", url4.clone());
+                                                    thread::park_timeout(::std::time::Duration::from_secs(0));
+                                                    tx_req2.send(None);
+                                                }
+                                            }
+                                            Err(e) => {
+                                                error!("fail to read is done flag, url: {}, reason: {:?}", url4.clone(), e);
+                                                tx_req2.send(None);
+                                            }
+                                        }
+
+                                    })
                                 });
 
                                 match rx_req.recv() {
@@ -115,8 +136,8 @@ impl<'a, T: 'a + Cache + Send> Resource for ImageResource<'a, T> {
                             }
                         };
 
-                        let url4 = url2.clone();
-                        info!("image url: {} reason: {}", url4, reason);
+                        let url5 = url2.clone();
+                        info!("image url: {} reason: {}", url5, reason);
                         let result_item = ChannelItem {
                             extra: Some(ChannelItemType::Image(ChannelImageItem { url: url2, bytes: result })),
                             result: reason,
