4fd555aa0a480c957845a18152ea2877a92ab3a6
u59u75u65 <merrymenfolks@gmail.com>
2017-04-04 13:58:34 +0800

    [FIX] prevent blocking in requester

--- src/web.rs
+++ src/web.rs
@@ -20,42 +20,43 @@ impl Requester {
 
         // web client
         thread::spawn(move || {
-            let mut wr = WebResource::new();
-            let mut fc = Box::new(FileCache::new());
-            let ct = CancellationTokenSource::new();
-            ct.cancel_after(::std::time::Duration::new(10, 0));
+
             while (*working).load(Ordering::Relaxed) {
                 match rx_req.recv() {
                     Ok(item) => {
-
-                        let th = thread::current();
-                        ct.run(|| { th.unpark(); }, || match item.extra.clone() {
-                            Some(o) => {
-                                info!("request: {:?}", o);
-                                match o {
-                                    ChannelItemType::Index(_) => {
-                                        let mut index_resource = IndexResource::new(&mut wr, &mut fc);
-                                        tx_res.send(index_resource.fetch(&item)).expect("[web client] fail to send index request");
-                                    }
-                                    ChannelItemType::Show(_) => {
-                                        let mut show_resource = ShowResource::new(&mut wr, &mut fc);
-                                        tx_res.send(show_resource.fetch(&item)).expect("[web client] fail to send show request");
-                                    }
-                                    ChannelItemType::Image(_) => {
-                                        let mut image_resource = ImageResource::new(&mut fc);
-                                        tx_res.send(image_resource.fetch(&item)).expect("[web client] fail to send image request");
+                        let mut wr = WebResource::new();
+                        let mut fc = Box::new(FileCache::new());
+                        let ct = CancellationTokenSource::new();
+                        ct.cancel_after(::std::time::Duration::new(10, 0));
+                        let tx_res2 = tx_res.clone();
+                        thread::spawn(move || {
+                            info!("[requester] #recv");
+                            let th = thread::current();
+                            ct.run(|| { th.unpark(); }, || match item.extra.clone() {
+                                Some(o) => {
+                                    info!("request: {:?}", o);
+                                    match o {
+                                        ChannelItemType::Index(_) => {
+                                            let mut index_resource = IndexResource::new(&mut wr, &mut fc);
+                                            tx_res2.send(index_resource.fetch(&item)).expect("[web client] fail to send index request");
+                                        }
+                                        ChannelItemType::Show(_) => {
+                                            let mut show_resource = ShowResource::new(&mut wr, &mut fc);
+                                            tx_res2.send(show_resource.fetch(&item)).expect("[web client] fail to send show request");
+                                        }
+                                        ChannelItemType::Image(_) => {
+                                            let mut image_resource = ImageResource::new(&mut fc);
+                                            tx_res2.send(image_resource.fetch(&item)).expect("[web client] fail to send image request");
+                                        }
                                     }
                                 }
-                            }
-                            None => { }
-                        });
+                                None => { tx_res2.send(Default::default()); }
+                            });
 
-                        if ct.is_canceled() {
-                            thread::park_timeout(::std::time::Duration::new(0, 250));
-                            // Err(OperationCanceled)
-                        } else {
-                            // Ok(())
-                        }
+                            if ct.is_canceled() {
+                                tx_res2.send(Default::default());
+                            }
+                        });
                     }
                     Err(_) => {}
                 }
