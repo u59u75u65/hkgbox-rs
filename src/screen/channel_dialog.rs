7f188f023e713852b3b1f5bccbc1d921ac24bc36
u59u75u65 <u59u75u65@gmail.com>
2026-04-27 04:16:04 +0800

    fix: remove redundant line at bottom of channel dialog
    Reduce dialog height from 18 to 17 to eliminate empty line
    between last channel and bottom border.

--- src/screen/channel_dialog.rs
+++ src/screen/channel_dialog.rs
@@ -66,7 +66,7 @@ impl ChannelDialog {
         let height = ::termion::terminal_size().expect("fail to get terminal size").1 as usize;
 
         let dialog_width = 35.min(width - 4);
-        let dialog_height = 18;
+        let dialog_height = 17;
 
         let dialog_x = ((width - dialog_width) / 2) as u16;
         let dialog_y = ((height - dialog_height) / 2) as u16;
@@ -105,7 +105,7 @@ impl ChannelDialog {
         // Channel list
         for (i, channel) in self.channels.iter().enumerate() {
             let line_num = i + 3;
-            if line_num > dialog_height - 3 {
+            if line_num >= dialog_height - 2 {
                 break;
             }
 
