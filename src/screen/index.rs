721ce6d7ce9edce625bc902e22726326373046e8
u59u75u65 <u59u75u65@gmail.com>
2026-04-29 03:20:16 +0800

    fix: prevent subtract overflow in index print_body
    - Use saturating_sub for author_spacing_width calculation
    - Prevents panic when author name length exceeds author_max_width
    - Fixes "attempt to subtract with overflow" panic at line 168
    - Author names can now be any length without crashing

--- src/screen/index.rs
+++ src/screen/index.rs
@@ -165,7 +165,7 @@ fn print_body(stdout: &mut ::termion::raw::RawTerminal<std::io::StdoutLock>,
         let title_spacing = (0..title_spacing_width).map(|_| " ").collect::<Vec<_>>().join("");
 
         let author = item.author.name.clone();
-        let author_spacing_width = author_max_width - jks_len(&author) + right_offset;
+        let author_spacing_width = author_max_width.saturating_sub(jks_len(&author)) + right_offset;
         let author_spacing = (0..author_spacing_width).map(|_| " ").collect::<Vec<_>>().join("");
 
         if selected_topic_index == i + 1 {
